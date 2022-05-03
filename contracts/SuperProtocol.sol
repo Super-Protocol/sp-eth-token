f0aa0ca8001a900396fc0d3b77241a09c038f4ab
Vk <vk@superprotocol.com>
2022-05-03 09:51:29 +0300

    Airdrop function has been made external.

--- contracts/SuperProtocol.sol
+++ contracts/SuperProtocol.sol
@@ -49,7 +49,7 @@ contract SuperProtocol is ERC20 {
         require(totalSupply() == 1_000_000_000 ether, "Something wrong in token distribution");
     }
 
-    function airdrop(AirdropInfo[] memory airdropInfos) public {
+    function airdrop(AirdropInfo[] memory airdropInfos) external {
         for (uint256 idx = 0; idx < airdropInfos.length; ++idx) {
             address receiver = airdropInfos[idx].receiver;
             uint256 amount = airdropInfos[idx].amount;
