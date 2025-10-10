4c786d77454bba92f8ff15f936690e25d23c3d2d
Vk <vk@superprotocol.com>
2025-10-10 10:52:31 +0300

    SP-7103. Review fixes.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -104,8 +104,8 @@ export default {
             },
         },
         local: {
-            url: config.rpcUrl,
-            account: config.deployerPrivateKey,
+            url: 'http://127.0.0.1:8545',
+            accounts: ['0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'],
         },
         opbnbTestnet: {
             chainId: 5611,
